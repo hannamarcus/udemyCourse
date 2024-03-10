b8f167b44d176714f8c3884c44c380002c31773a
Hanna Marcus <106893601+hannamarcus@users.noreply.github.com>
2024-03-09 18:39:34 -0800

    march 9 daily challenge

--- dailyChallenge.js
+++ dailyChallenge.js
@@ -12,3 +12,32 @@ function fibonacci(n) {
 }
 
 console.log(fibonacci(3)); // should output 2
+
+// march 6th
+
+function countVowels(str) {
+    let count = 0;
+    const cleanWord = str.toLowerCase();
+    for (let i = 0; i < cleanWord.length; i++) {
+        if ('aeiou'.includes(cleanWord[i])) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(countVowels("Hello World")); // Example usage
+
+function findLongestWord(str) {
+    const words = str.split(' ');
+    let longestWord = words[0];
+    for (let i = 1; i < words.length; i++) {
+        if (words[i].length > longestWord.length) {
+            longestWord = words[i];
+        }
+    }
+    return longestWord;
+}
+
+console.log(findLongestWord("Javascript is awesome!")); // Should output "Javascript"
+
